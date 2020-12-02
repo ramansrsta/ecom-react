aa378bb6a8a91ce45d8a3132cd676bb22e92e0e0
ramansrsta <ramansresta@gmail.com>
2020-12-02 16:07:31 +0545

    [Homepage completed ..with components and pages]

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.scss';
+
+import Directory from '../../components/directory/directory';
+
+const HomepageComponent = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+
+export default HomepageComponent;
